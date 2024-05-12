f5338d137caacb7a002a075fb8fb41469e7c7322
Ender-Fake <segushkadno5576@gmail.com>
2024-05-12 03:37:43 +0300

    init MinecraftAccessor for startUseItem method

---
+++ src/main/java/com/enderium/smartblockplacement/mixin/MinecraftAccessor.java
@@ -0,0 +1,11 @@
+package com.enderium.smartblockplacement.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Minecraft.class)
+public interface MinecraftAccessor {
+    @Invoker
+    void callStartUseItem ();
+}
